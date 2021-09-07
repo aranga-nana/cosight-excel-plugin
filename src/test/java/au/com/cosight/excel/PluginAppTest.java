11d3f84d06f4031b127eb5ac3b5ab565792f1da8
arangananayakkara <arangananayakkara@fintrix.com.au>
2021-09-07 15:50:12 +1000

    initial commit

---
+++ src/test/java/au/com/cosight/excel/PluginAppTest.java
@@ -0,0 +1,18 @@
+package au.com.cosight.excel;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PluginAppTest {
+
+    @Test
+    void loadContext(){
+        assertTrue(true);
+    }
+}
